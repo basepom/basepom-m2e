c6d56ae7921cb1ba89423bb39b639423f9fc42ac
Henning Schmiedehausen <henning@schmiedehausen.org>
2015-10-30 18:06:21 -0700

    update releasing script

--- scripts/releasing.sh
+++ scripts/releasing.sh
@@ -5,7 +5,7 @@ export SNAPSHOT=$2
 find . -name MANIFEST.MF -o -name feature.xml | xargs perl -pi -e "s|${RELEASE}.qualifier|${RELEASE}|"
 git add -u
 git commit -m "set osgi versions to ${RELEASE} for release"
-mvn clean release:clean release:prepare release:perform
+mvn -B -DreleaseVersion=$RELEASE -DdevelopmentVersion=$SNAPSHOT clean release:clean release:prepare release:perform
 git push
 git push --tags
 find . -name MANIFEST.MF -o -name feature.xml | xargs perl -pi -e "s|${RELEASE}|${SNAPSHOT}.qualifier|"
